b1f18cb188efb8d32b1e19da60e9374c2b270543
siwar142001 <selmisiwar3@gmail.com>
2025-11-26 19:39:41 +0100

    fix bug

--- app/Http/Requests/Organization/StoreOrganization.php
+++ app/Http/Requests/Organization/StoreOrganization.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Organization;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreOrganization extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreOrganization extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->check();
+        return Auth::check();
     }
 
     /**
